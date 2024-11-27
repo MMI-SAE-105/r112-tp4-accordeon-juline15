52a0a1b2c35569b482cb6d47c13fe47b21f5c3c6
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2024-11-27 11:38:16 +0100

    ajout du code JS

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,19 @@
 
 const accordeon = document.querySelector(".accordeon");
 accordeon.querySelectorAll("details").forEach((det) => (det.open = false));
+
+details.forEach(det => det.open = false);
+
+
+details.forEach(detail => {
+    detail.addEventListener('toggle', () => {
+        if (detail.open) {
+
+            details.forEach(otherDetail => {
+                if (otherDetail !== detail && otherDetail.open) {
+                    otherDetail.open = false;
+                }
+            });
+        }
+    });
+});
